6550afe4da77aa1629ba4677f0412358068982b7
François Hodierne <francois@hodierne.net>
2022-07-22 08:40:00 +0200

    remove default bcc

--- server/lib/email.ts
+++ server/lib/email.ts
@@ -125,8 +125,6 @@ const sendMessage = (
   html: string,
   options: SendMessageOptions = {},
 ) => {
-  options.bcc = options.bcc || 'emailbcc@opencollective.com';
-
   if (!isArray(recipients)) {
     recipients = [recipients];
   }
